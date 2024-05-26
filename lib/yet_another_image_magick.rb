94489649cd3fd0222d4a9b50224ac5abcdf0e84d
Коплик Дмитрий <koplikdima2004@gmail.com>
2024-05-26 10:47:50 +0300

    gem init

---
+++ lib/yet_another_image_magick.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "yet_another_image_magick/version"
+
+module YetAnotherImageMagick
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "yet_another_image_magick"
+
+require "minitest/autorun"
